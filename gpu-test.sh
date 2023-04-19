a9528785e375f01a06d0d27bfacf098162079fa1
nakhlarafi <nakhla054@gmail.com>
2023-04-19 18:19:21 -0400

    batch file added

--- gpu-test.sh
+++ gpu-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SBATCH -J Grace --mem=100M --account=r_mdnakh
-SBATCH --gpus=40gb:1
+#SBATCH -J Grace --mem=100M --account=r_mdnakh
+#SBATCH --gpus=40gb:1
 python3 runtotal.py Time
 nvidia-smi -L
 sleep 20
