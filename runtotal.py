6dc85092f97a5ae24584a011a022e2da39b92f56
nakhlarafi <nakhla054@gmail.com>
2023-06-18 16:41:01 -0400

    data distributed parallel feature try

--- runtotal.py
+++ runtotal.py
@@ -4,7 +4,6 @@ import time
 import os, sys
 import pickle
 project = sys.argv[1]
-# os.environ["CUDA_VISIBLE_DEVICES"] = "1,2,3"
 card = [0]
 lst = list(range(len(pickle.load(open(project + '.pkl', 'rb')))))
 singlenums = {'Time':5, 'Math':2, "Lang":1, "Chart":3, "Mockito":4, "Closure":1}
@@ -20,7 +19,7 @@ for i in tqdm(range(int(len(lst) / totalnum) + 1)):
             continue
         cardn =int(j / singlenum)
         # p = subprocess.Popen("CUDA_VISIBLE_DEVICES="+str(card[cardn]) + " python3 run.py %d %s %f %d %d"%(lst[totalnum * i + j], project, lr, seed, batch_size), shell=True)
-        p = subprocess.Popen("torchrun --nnodes=1 --nproc_per_node=1 --rdzv_id=100 --rdzv_backend=c10d --rdzv_endpoint=virya4:29407 run.py %d %s %f %d %d"%(lst[totalnum * i + j], project, lr, seed, batch_size), shell=True)
+        p = subprocess.Popen("CUDA_VISIBLE_DEVICES=1 torchrun --nnodes=1 --nproc_per_node=1 --rdzv_id=100 --rdzv_backend=c10d --rdzv_endpoint=virya4:29407 run.py %d %s %f %d %d"%(lst[totalnum * i + j], project, lr, seed, batch_size), shell=True)
         jobs.append(p)
         time.sleep(10)
     for p in jobs:
