6e38369844059b51db07ba3cf82ccab56c61914f
nakhlarafi <nakhla054@gmail.com>
2023-06-19 19:06:34 -0400

    data distributed parallel feature try

--- runtotal.py
+++ runtotal.py
@@ -1,31 +1,34 @@
 import subprocess
 from tqdm import tqdm
-import time
 import os, sys
 import pickle
 
 project = sys.argv[1]
-card = [0,1]
 lst = list(range(len(pickle.load(open(project + '.pkl', 'rb')))))
-singlenums = {'Time':5, 'Math':2, "Lang":1, "Chart":3, "Mockito":4, "Closure":1}
+singlenums = {'Time':5, 'Math':2, "Lang":10, "Chart":3, "Mockito":4, "Closure":1}
 singlenum = singlenums[project]
-totalnum = len(card) * singlenum
 lr = 1e-2
 seed = 0
 batch_size = 60
-for i in tqdm(range(int(len(lst) / totalnum) + 1)):
-    jobs = []
-    for j in range(totalnum):
-        if totalnum * i + j >= len(lst):
-            continue
-        cardn =int(j / singlenum)
-        # p = subprocess.Popen("CUDA_VISIBLE_DEVICES="+str(card[cardn]) + " python3 run.py %d %s %f %d %d"%(lst[totalnum * i + j], project, lr, seed, batch_size), shell=True)
-        # p = subprocess.Popen("export MASTER_ADDR=$(scontrol show hostname ${SLURM_NODELIST} | head -n 1)")
-        p = subprocess.Popen("CUDA_VISIBLE_DEVICES=0 torchrun --nnodes=1 --nproc_per_node=1 --rdzv_id=100 --rdzv_backend=c10d --rdzv_endpoint=virya4:29405 run.py %d %s %f %d %d"%(lst[totalnum * i + j], project, lr, seed, batch_size), shell=True)
-        jobs.append(p)
-        time.sleep(10)
-    for p in jobs:
-        p.wait()
-p = subprocess.Popen("python3 sum.py %s %d %f %d"%(project, seed, lr, batch_size), shell=True)
+
+# calculate total number of jobs for each GPU
+totalnum = len(lst)
+gpu_count = 1  # You mentioned you have 3 GPUs
+
+# Iterate through all jobs
+for i in tqdm(range(totalnum)):
+    # Calculate which GPU to use for this job
+    gpu_id = i % gpu_count
+
+    # Start the job on the selected GPU
+    command = "CUDA_VISIBLE_DEVICES=%d python run.py %d %s %f %d %d" % (gpu_id, lst[i], project, lr, seed, batch_size)
+    p = subprocess.Popen(command, shell=True)
+
+    # Wait for the job to finish before continuing
+    p.wait()
+
+# Run the summation and watch scripts after all jobs are done
+p = subprocess.Popen("python sum.py %s %d %f %d" % (project, seed, lr, batch_size), shell=True)
 p.wait()
-subprocess.Popen("python3 watch.py %s %d %f %d"%(project, seed, lr, batch_size),shell=True)            
+
+subprocess.Popen("python watch.py %s %d %f %d" % (project, seed, lr, batch_size), shell=True)
