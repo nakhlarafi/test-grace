bb844b9fa77f032469b7937e03251e909f05bc6d
nakhlarafi <nakhla054@gmail.com>
2023-06-20 11:25:54 -0400

    data distributed parallel feature try

--- runtotal.py
+++ runtotal.py
@@ -1,34 +1,63 @@
+# import subprocess
+# from tqdm import tqdm
+# import os, sys
+# import pickle
+
+# project = sys.argv[1]
+# lst = list(range(len(pickle.load(open(project + '.pkl', 'rb')))))
+# singlenums = {'Time':5, 'Math':2, "Lang":10, "Chart":3, "Mockito":4, "Closure":1}
+# singlenum = singlenums[project]
+# lr = 1e-2
+# seed = 0
+# batch_size = 60
+
+# # calculate total number of jobs for each GPU
+# totalnum = len(lst)
+# gpu_count = 1  # You mentioned you have 3 GPUs
+
+# # Iterate through all jobs
+# for i in tqdm(range(totalnum)):
+#     # Calculate which GPU to use for this job
+#     gpu_id = i % gpu_count
+
+#     # Start the job on the selected GPU
+#     command = "CUDA_VISIBLE_DEVICES=%d python run.py %d %s %f %d %d" % (gpu_id, lst[i], project, lr, seed, batch_size)
+#     p = subprocess.Popen(command, shell=True)
+
+#     # Wait for the job to finish before continuing
+#     p.wait()
+
+# # Run the summation and watch scripts after all jobs are done
+# p = subprocess.Popen("python sum.py %s %d %f %d" % (project, seed, lr, batch_size), shell=True)
+# p.wait()
+
+# subprocess.Popen("python watch.py %s %d %f %d" % (project, seed, lr, batch_size), shell=True)
+
 import subprocess
 from tqdm import tqdm
+import time
 import os, sys
 import pickle
 
 project = sys.argv[1]
+card = [0]
 lst = list(range(len(pickle.load(open(project + '.pkl', 'rb')))))
 singlenums = {'Time':5, 'Math':2, "Lang":10, "Chart":3, "Mockito":4, "Closure":1}
 singlenum = singlenums[project]
+totalnum = len(card) * singlenum
 lr = 1e-2
 seed = 0
 batch_size = 60
 
-# calculate total number of jobs for each GPU
-totalnum = len(lst)
-gpu_count = 1  # You mentioned you have 3 GPUs
+for i in tqdm(range(int(len(lst) / totalnum) + 1)):
+    for j in range(totalnum):
+        if totalnum * i + j >= len(lst):
+            continue
+        cardn =int(j / singlenum)
+        p = subprocess.Popen("CUDA_VISIBLE_DEVICES="+str(card[cardn]) + " python3 run.py %d %s %f %d %d"%(lst[totalnum * i + j], project, lr, seed, batch_size), shell=True)
+        p.wait()
 
-# Iterate through all jobs
-for i in tqdm(range(totalnum)):
-    # Calculate which GPU to use for this job
-    gpu_id = i % gpu_count
-
-    # Start the job on the selected GPU
-    command = "CUDA_VISIBLE_DEVICES=%d python run.py %d %s %f %d %d" % (gpu_id, lst[i], project, lr, seed, batch_size)
-    p = subprocess.Popen(command, shell=True)
-
-    # Wait for the job to finish before continuing
-    p.wait()
-
-# Run the summation and watch scripts after all jobs are done
-p = subprocess.Popen("python sum.py %s %d %f %d" % (project, seed, lr, batch_size), shell=True)
+p = subprocess.Popen("python3 sum.py %s %d %f %d"%(project, seed, lr, batch_size), shell=True)
 p.wait()
 
-subprocess.Popen("python watch.py %s %d %f %d" % (project, seed, lr, batch_size), shell=True)
+subprocess.Popen("python3 watch.py %s %d %f %d"%(project, seed, lr, batch_size),shell=True)            
